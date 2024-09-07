80b0861d1267577cc886ccfa4b71e6318e40a488
XiaoSuan <357770456@qq.com>
2024-09-07 11:04:36 +0800

    feat: 修改页脚

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,7 +4,8 @@ export default defineConfig({
   themeConfig: {
     logo: '/logo.png',
     name: '天书奇谈',
-    prefersColor: { default: 'light', switch: false }
+    prefersColor: { default: 'light', switch: false },
+    footer: 'Created by 无情的毒'
   },
   favicons: ['/logo.png']
 });
