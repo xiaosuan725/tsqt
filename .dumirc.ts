eab6c19f54266d0d312abc7d2ed75be592503dda
XiaoSuan <357770456@qq.com>
2024-09-07 14:34:31 +0800

    feat: 修改页脚

--- .dumirc.ts
+++ .dumirc.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     logo: '/logo.png',
     name: '天书奇谈',
     prefersColor: { default: 'light', switch: false },
-    footer: 'Created by 无情的毒'
+    footer: '内容非原创，网站由清仙烹（无情的毒，我喜欢跳，我喜欢Rap，-2，等人）制作，主要是无情的毒做的'
   },
   favicons: ['/logo.png']
 });
